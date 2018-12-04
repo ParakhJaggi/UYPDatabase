7e9795c1aad368674f836cba0f03a2fe7d5c76f4
Mork <brandon_mork@baylor.edu>
2018-12-04 15:58:24 -0600

    Changed website title, tab title, and removed code not being used

--- src/main/java/UYPDatabase/site/common/guest/GuestDao.java
+++ src/main/java/UYPDatabase/site/common/guest/GuestDao.java
@@ -3,9 +3,6 @@ package UYPDatabase.site.common.guest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-/**
- * Created by jlutteringer on 8/23/17.
- */
 @Repository
 public class GuestDao {
 
