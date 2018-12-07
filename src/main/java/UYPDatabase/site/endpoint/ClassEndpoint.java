aa146a2b5b44943ab1faf073e812e3e3009edc73
Mork <brandon_mork@baylor.edu>
2018-12-07 00:06:32 -0600

    Added my class page

--- src/main/java/UYPDatabase/site/endpoint/ClassEndpoint.java
+++ src/main/java/UYPDatabase/site/endpoint/ClassEndpoint.java
@@ -20,8 +20,18 @@ public class ClassEndpoint {
         return userService.getClasses();
     }
 
-    @PostMapping(value = "/register")
-    public void registerClass(@RequestBody String username, @RequestBody Integer classID){
+    @GetMapping(value = "/{username}")
+    public ClassDto getClasses(@PathVariable("username") String username) throws SQLException, ClassNotFoundException {
+        return userService.getClasses(username);
+    }
+
+    @PostMapping(value = "/register/{username}/{classID}")
+    public void registerClass(@PathVariable("username") String username, @PathVariable("classID") Integer classID) throws SQLException, ClassNotFoundException {
         userService.registerClass(username, classID);
+    }
+
+    @PostMapping(value = "/drop/{username}/{classID}")
+    public void dropClass(@PathVariable("username") String username, @PathVariable("classID") Integer classID) throws SQLException, ClassNotFoundException {
+        userService.dropClass(username, classID);
     }
 }
